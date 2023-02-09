2a1a4eeaf4acbe14f09fedb92779aca372c87d55
Vinayak Sudhakar Kone <83850932+vinayak-k24@users.noreply.github.com>
2023-02-09 20:21:38 +0530

    Done

---
+++ 1_LCS.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    string str1,str2;   // Defining two strings
+
+    cin>>str1;          // Taking input strings
+    cin>>str2;
+
+    len1=str1.size();   // Assigning the length of the strings to len1 and len2
+    len2=str2.size();
+
+    vector<vector <int>>  LCS[len1+1][len2+1];
+
+
+}
