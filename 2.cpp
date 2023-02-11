707e8130611e47e272084a63495ddcea461df3f6
Vinayak Kone <83850932+vinayak-k24@users.noreply.github.com>
2023-02-11 18:47:34 +0530

    Done

---
+++ 2.cpp
@@ -0,0 +1,37 @@
+// APS 2023 Hand-out 01
+
+// What does the below code do ?
+
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num[10000];
+    int i,n,j,k,temp;
+
+    cout<<"Enter n";
+    cin>>n;
+    cout<<endl;
+
+    cout<<"Enter n numbers:"<<endl;
+    for(int i=0;i<n;i++)
+        cin>>num[i];
+    cout<<endl;
+
+    for(j=1;j<=n;j++)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            temp = num[i];
+            num[i] = num[i+1];
+            num[i+1] = temp;
+
+            for(k=0;k<n;k++)
+                cout<<num[k]<<" ";
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
