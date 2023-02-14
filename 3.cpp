a1fc260b40d2fbad60af057fef03bdc85e7b2c46
Vinayak Kone <83850932+vinayak-k24@users.noreply.github.com>
2023-02-14 10:34:07 +0530

    DOne

---
+++ 3.cpp
@@ -0,0 +1,67 @@
+// n job n 
+
+/*
+input
+3
+3 2 7
+5 1 3
+2 7 2
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define l0 long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cout<<"Enter n value : ";
+    cin>>n;
+    int cost[n][n];
+
+    cout<<"Enter the values of the cost matrix -"<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>cost[i][j];
+        }
+    }
+
+    vector<int> dp(pow(2,n),INT_MAX);
+    dp[0]=0;
+
+    for(int mask=0;mask<pow(2,n);mask++)
+    {
+        int x=0;
+        int temp=mask;
+
+        while(temp)
+        {
+            temp&=(temp-1);
+            x++;
+        }
+
+        for(int j=0;j<n;j++)
+        {
+            if(!(mask & (1 << j)))
+            {
+                dp[mask | 1<<j]=min(dp[mask | 1<<j], dp[mask]+cost[x][j]);
+            }
+        }
+    }
+
+    for(int i=0;i<pow(2,n);i++)
+    {
+        cout<<dp[i]<<" ";
+
+    }
+
+    cout<<endl<<dp[pow(2,n)-1];
+
+    return 0;
+}
